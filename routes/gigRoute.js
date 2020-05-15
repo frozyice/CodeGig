019c1580b580a5856855860a6cf489c2c8d76e4a
Karmo <Karmo.Kiima@Eesti.ee>
2020-05-15 19:56:03 +0300

    Add handlebars, update router

--- routes/gigRoute.js
+++ routes/gigRoute.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../modules/Gig')
 
-router.get('/', (req,res) =>
+router.get('/', (req, res) =>
     Gig.findAll()
-    .then(gigs => {
-    console.log(gigs);
-    res.sendStatus(200);
-    })
-    .catch(err => console.log(err)));
+        .then(gigs => {
+            console.log(gigs);
+            res.render('gigs', {
+                gigs
+            });
+        })
+        .catch(err => console.log(err)));
 
-router.get('/add', (req,res) =>{
+router.get('/add', (req,res) => res.render('add'));
+
+router.post('/add', (req, res) => {
     const data = {
         title: 'dummy',
         technologies: 'dummy',
@@ -20,13 +24,13 @@ router.get('/add', (req,res) =>{
         contact_email: 'dummy'
     }
 
-    let { title, technologies, budget, description, contact_email} = data;
-    
+    let { title, technologies, budget, description, contact_email } = data;
+
     Gig.create({
         title, technologies, budget, description, contact_email
     })
-    .then(gig => res.redirect('/gigs'))
-    .catch(err => console.log(err));
+        .then(gig => res.redirect('/gigs'))
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const db = require('./config/database');
 db.authenticate().then(() => console.log('db OK'));
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('INDEX'));
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('index', {layout: 'landing'}));
 app.use('/gigs', require('./routes/gigRoute'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server started port ${port}`));
